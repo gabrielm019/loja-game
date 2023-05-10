5a1dd9c951ec7488196c575794d957abc9e5d86a
Gabriel Augusto <110576161+gabrielm019@users.noreply.github.com>
2023-05-10 19:46:33 -0300

    .

--- listar-jogos.php
+++ listar-jogos.php
@@ -1,83 +1,80 @@
 <?php
-
 include "cabecalho.php";
-
 include "menu-sistema.php";
-
 ?>
 
 <div class="container">
-
     <div class="row">
-
         <div class="col">
-
             <h1>Lista de jogos</h1>
-
         </div>
-
     </div>
-
     <div class="row">
-
         <div class="col">
-
-            <table>
-                <td>Código</td>
-                <td>Título</td>
-                <td>Categoria</td>
-                <td>Açoes</td>
-
-                <?php
-
-                include "conexao.php";
-
-                $sql_buscar = "select * from jogo";
-
-                $todos_os_jogos = mysqli_query($conexao, $sql_buscar);
-
-                while ($um_jogo = mysqli_fetch_assoc($todos_os_jogos)) :
-                ?>
-
+            <table class="table table-hover table-striped">
+                <thead>
                     <tr>
-
-                        <td><?php echo $um_jogo["id"];?></td>
-
-                        <td><?php echo $um_jogo["titulo"];?></td>
-
-                        <td><?php echo $um_jogo["categoria"];?></td>
-
-                       <a href="excluir-jogos.php?id=<?php echo $um_jogo["id"];?>">
-                       <img src="excluir.png" width="20">
-                       </a>
-
-
+                        <th>Código</th>
+                        <th>Título</th>
+                        <th>Categoria</th>
+                        <th>Ações</th>
                     </tr>
-
-                <?php
-
-
-                endwhile;
-
-                mysqli_close($conexao);
-
-                ?>
+                </thead>
+                <tbody>
+                    <?php
+                    include "conexao.php";
+                    $sql_buscar = "select * from jogo";
+                    $todos_os_jogos = mysqli_query($conexao, $sql_buscar);
+                    while ($um_jogo = mysqli_fetch_assoc($todos_os_jogos)) :
+                    ?>
+                        <tr>
+                            <td> <?php echo $um_jogo["id"]; ?></td>
+                            <td> <?php echo $um_jogo["titulo"]; ?> </td>
+                            <td> <?php echo $um_jogo["categoria"]; ?> </td>
+                            <td>
+                                <a href="excluir-jogos.php?id=<?php echo $um_jogo["id"]; ?>">
+                                    <img src="excluir.png" width="20">
+                                </a>
+                            </td>
+                        </tr>
+                    <?php
+                    endwhile;
+                    mysqli_close($conexao);
+                    ?>
+                </tbody>
             </table>
         </div>
     </div>
+    <?php
+
+    $msg = $_GET['msg'] ?? "";
+
+    if ($msg == "excluido") {
+    ?>
+        <div class="toast-container position-fixed bottom-0 end-0 p-3">
+            <div id="liveToast" class="toast bg-danger" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="toast-header">
+                    <img src="excluir.png" width="24" class="rounded me-2" alt="...">
+                    <strong class="me-auto">ATENÇÃO</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body text-white">
+                    Jogo excluído com sucesso!
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
 </div>
 
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
-
-
+<script>
+    const toastLiveExample = document.getElementById('liveToast')
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+</script>
 
 </body>
-
-
-
 
 </html>
--- menu-sistema.php
+++ menu-sistema.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                <li class="nav-item">
-                <a class="nav-link" href="listar-jogos.php">jogos</a>
+                <a class="nav-link" href="listar-jogos.php">Jogos</a>
                </li>
                <li class="nav-item">
                 <a class="nav-link" href="listar-usuarios.php">Usuário</a>
